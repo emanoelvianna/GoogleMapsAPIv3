c602417f7f74f86cfa88c3ae8f4598380ce5ca40
emanoelvianna <emanoel.vianna@outlook.com>
2015-07-12 17:56:50 -0300

    modificações nos ponto de exemplo.

--- ApplicacaoAPIGoogle/js/map.js
+++ ApplicacaoAPIGoogle/js/map.js
@@ -1,14 +1,6 @@
 
 var map;
 
-var locations = [
-        [-51.12794467539000000,-30.04794290572600000, 1],
-        [-51.23682367539000000,-30.09264490572600000, 2],
-        [-51.15110967539000000,-30.02610290572600000, 3],
-        [-51.14673067539000000,-30.02404990572600000, 4]
-    ]; 
-
-
 function initialize() {
     var position = new google.maps.LatLng(-30.05, -51.18);
 
@@ -24,23 +16,27 @@ function initialize() {
 }
 
 var image = {
-    url: 'imag/marcador.png'
+    url: 'img/marcador.png'
 };
+
+var locations = [
+        ['Hospital', -30.039119, -51.207156, 1],
+];
 
 function points(map,locations) {
     
     
-    for( var i = 0; i < locations.length; i++) {
-        var beach = locations[i];
-        var myLatLng = new google.maps.LatLng(beach[1], beach[2]);
-        
+    for (var i = 0; i < locations.length; i++) {
+    var beach = locations[i];
+    var myLatLng = new google.maps.LatLng(beach[1], beach[2]);
+    
         var marker = new google.maps.Marker({
-            position: myLatLng,
-            map: map,
-            title: 'Hello World!'
-        });
+        position: myLatLng,
+        map: map,
+        icon: image
+    });
         
-    }
+  }
 
 }
 
