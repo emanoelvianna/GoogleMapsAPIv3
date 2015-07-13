028e3488b9c834efc09063f8c49bca9310c4892e
emanoelvianna <emanoel.vianna@outlook.com>
2015-07-12 21:19:12 -0300

    app01.01 - plotando no mapa, ponto e desenho do quadrado.

--- ApplicacaoAPIGoogle/js/map.js
+++ ApplicacaoAPIGoogle/js/map.js
@@ -1,5 +1,6 @@
 
 var map;
+var rectangle;
 
 function initialize() {
     var position = new google.maps.LatLng(-30.05, -51.18);
@@ -12,7 +13,27 @@ function initialize() {
 
     map = new google.maps.Map(document.getElementById("mapa"), options);
     
-    points(map, locations);    
+    
+    var bounds = new google.maps.LatLngBounds(
+      new google.maps.LatLng(-30.10, -51.249),
+      new google.maps.LatLng(-30.00, -51.143)
+    );
+    
+    // Define the rectangle and set its editable property to true.
+    rectangle = new google.maps.Rectangle({
+        bounds: bounds,
+        editable: true,
+        draggable: true
+    });
+    
+    
+    rectangle.setMap(map);
+    points(map, locations); 
+
+    google.maps.event.addListener(rectangle, 'bounds_changed', showNewRect);
+    
+    
+       
 }
 
 var image = {
@@ -40,8 +61,17 @@ function points(map,locations) {
 
 }
 
+function d() {
+    window.onload = function() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var img = document.getElementById("scream");
+    ctx.drawImage(img, 10, 10);
+    }   
+}
 
 //initialize();
 //pontos();
 google.maps.event.addDomListener(window, 'load', initialize);
+
 
